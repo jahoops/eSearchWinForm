a606605562b791a2df4bc10fcc79aa481e0f7776
Lord Melbury <jwhooper@gmail.com>
2020-07-05 01:28:26 -0400

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Controls.Add(this.flowLayoutPanel2);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel2.BackColor = System.Drawing.Color.Gray;
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(800, 30);
+            this.flowLayoutPanel2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "eSearch";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,165 @@
+﻿using Fizzler.Systems.HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadSearches();
+        }
+
+        private void LoadSearches()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\eSearch\";
+            string[] files = Directory.GetFiles(path, "*.txt");
+            var buttonArray = new Button[files.Length];
+            for (int i = 0; i < files.Length; i++)
+            {
+                var file = files[i];
+                var filename = Path.GetFileName(file);
+                buttonArray[i] = new Button
+                {
+                    Text = filename,
+                    ForeColor = Color.Black,
+                    BackColor = Color.White,
+                    AutoSize = true
+                };
+                buttonArray[i].Click += (s1, e1) =>
+                {
+                    LoadSearch(file);
+                };
+            }
+            flowLayoutPanel2.Controls.AddRange(buttonArray);
+            Application.DoEvents();
+        }
+        private void LoadSearch(string filePath)
+        {
+            string[] lines = System.IO.File.ReadAllLines(filePath);
+            Button refresh = new Button
+            {
+                Text = "Refresh All",
+                ForeColor = Color.White,
+                BackColor = Color.LightSkyBlue,
+                AutoSize = true
+            };
+            refresh.Click += (s, e) =>
+            {
+                var web = new HtmlAgilityPack.HtmlWeb();
+                var document = new HtmlAgilityPack.HtmlDocument();
+                var cnt = flowLayoutPanel1.Controls.Count;
+                for (int i = 3; i < cnt; i++)
+                {
+                    Button b = flowLayoutPanel1.Controls[i] as Button;
+                    Color saveColor = b.BackColor;
+                    b.BackColor = Color.Yellow;
+                    b.Refresh();
+                    Application.DoEvents();
+                    try
+                    {
+                        document = web.Load("https://www.ebay.com/sch/i.html?_from=R40&_sacat=0&_nkw=" + b.Text);
+                        var page = document.DocumentNode;
+                        var count = page.QuerySelector(".srp-controls__count-heading span:first-child").InnerText;
+                        if (count != "0")
+                        {
+                            b.BackColor = Color.LightGreen;
+                        }
+                        else
+                        {
+                            b.BackColor = saveColor;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        b.BackColor = Color.Pink;
+                    }
+                    Application.DoEvents();
+                }
+            };
+            flowLayoutPanel1.Controls.Add(refresh);
+            Button openfound = new Button
+            {
+                Text = "Open Found",
+                ForeColor = Color.White,
+                BackColor = Color.LightGreen,
+                AutoSize = true
+            };
+            openfound.Click += (s, e) =>
+            {
+                var cnt = flowLayoutPanel1.Controls.Count;
+                for (int i = 3; i < cnt; i++)
+                {
+                    Button b = flowLayoutPanel1.Controls[i] as Button;
+                    if (b.BackColor == Color.LightGreen)
+                    {
+                        System.Diagnostics.ProcessStartInfo sInfo = new ProcessStartInfo("https://www.ebay.com/sch/i.html?_from=R40&_sacat=0&_nkw=" + b.Text);
+                        Process.Start(sInfo);
+                    }
+                }
+            };
+            flowLayoutPanel1.Controls.Add(openfound);
+            Button openerrors = new Button
+            {
+                Text = "Open Errors",
+                ForeColor = Color.White,
+                BackColor = Color.Pink,
+                AutoSize = true
+            };
+            openerrors.Click += (s, e) =>
+            {
+                var cnt = flowLayoutPanel1.Controls.Count;
+                for (int i = 3; i < cnt; i++)
+                {
+                    Button b = flowLayoutPanel1.Controls[i] as Button;
+                    if (b.BackColor == Color.Pink)
+                    {
+                        System.Diagnostics.ProcessStartInfo sInfo = new ProcessStartInfo("https://www.ebay.com/sch/i.html?_from=R40&_sacat=0&_nkw=" + b.Text);
+                        Process.Start(sInfo);
+                    }
+                }
+            };
+            flowLayoutPanel1.Controls.Add(openerrors);
+            Application.DoEvents();
+            var buttonArray = new Button[lines.Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                String s = line.Replace("\"", "'");
+                s = s.Replace("\n", " ");
+                s = s.Replace("\r", " ");
+                s = s.Replace("\t", " ");
+                s = s.Replace("\"", "'");
+                s = Regex.Replace(s, @"\s+", " ");
+                s = s.Trim();
+                buttonArray[i] = new Button
+                {
+                    Text = s,
+                    AutoSize = true
+                };
+                buttonArray[i].Click += (s1, e1) =>
+                {
+                    System.Diagnostics.ProcessStartInfo sInfo = new ProcessStartInfo("https://www.ebay.com/sch/i.html?_from=R40&_sacat=0&_nkw=" + s);
+                    Process.Start(sInfo);
+                };
+            }
+            flowLayoutPanel1.Controls.AddRange(buttonArray);
+            Application.DoEvents();
+        }
+    }
+}
+ 
